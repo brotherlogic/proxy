5ca8e3530fbf976411b66cb5c307de6e88c47d73
Simon Tucker <brotherlogic@gmail.com>
2020-10-17 09:28:27 -0700

    Cleans state

--- proxy.go
+++ proxy.go
@@ -71,9 +71,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{
-		&pbg.State{Key: "yep", Value: int64(8)},
-	}
+	return []*pbg.State{}
 }
 
 var (
