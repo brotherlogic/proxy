2a8c27918bef946a2a391e5147085e4dc766140b
Simon Tucker <brotherlogic@gmail.com>
2020-10-17 08:53:02 -0700

    Turns off status logging

--- proxyapi.go
+++ proxyapi.go
@@ -28,7 +28,7 @@ func (s *Server) AddLocation(ctx context.Context, req *pb.AddLocationRequest) (*
 
 // RecordStatus records status
 func (s *Server) RecordStatus(ctx context.Context, req *pbft.StatusRequest) (*pbft.StatusResponse, error) {
-	s.Log(fmt.Sprintf("Received status %v", req))
+	//s.Log(fmt.Sprintf("Received status %v", req))
 	conn, err := s.FDialServer(ctx, "frametracker")
 	if err != nil {
 		return &pbft.StatusResponse{}, err
